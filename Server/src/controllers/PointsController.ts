a2bdefe49bce66bc4fe7c65040b6dac61cb10ec8
Rodrigo <rodrigodiasf@gmail.com>
2020-06-04 13:44:38 +0100

    Adicionado o layout da página criar ponto de coleta e maps

--- Server/src/controllers/PointsController.ts
+++ Server/src/controllers/PointsController.ts
@@ -16,7 +16,7 @@ class PointsController {
   
     const trx = await knex.transaction();
     const point= {
-      image:'image-fake',
+      image:'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name, 
       email, 
       whatsapp, 
---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+import {Map, TileLayer, Marker} from 'react-leaflet';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+const CreatePoint = () => {
+  function handleInputChange(){
+    
+  }
+  return(
+   <div id="page-create-point">
+     <header>
+       <img src={logo} alt="Ecoleta"/>
+       <Link to="/">
+         <FiArrowLeft/>
+         Voltar para home
+       </Link>
+     </header>
+     <form>
+       <h1>Cadastro do <br/> ponto de coleta </h1>
+       <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </fieldset>
+
+       <fieldset>
+         <legend>
+           <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+         </legend>
+          <Map center={[38.6560185, -9.0633101]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[38.6560185, -9.0633101]}/>
+          </Map>
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="uf">Estado (UF)</label>
+                <select name="uf" id="uf">
+                  <option value="0">Selecione um Estado </option>
+                </select>
+              </div>
+            </div>
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="city">Cidade</label>
+                <select name="city" id="city">
+                  <option value="0">Selecione uma cidade </option>
+                </select>
+              </div>
+            </div>
+       </fieldset>
+
+       <fieldset>
+         <legend>
+           <h2>Itens de coleta</h2>
+           <span>Selecione o endereço no mapa</span>
+         </legend>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+         <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
+              <span>Lâmpadas</span>
+            </li>
+         </ul>
+       </fieldset>
+       <button type="submit">Cadastrar ponto de coleta</button>
+     </form>
+   </div>
+  );
+}
+
+export default CreatePoint;
