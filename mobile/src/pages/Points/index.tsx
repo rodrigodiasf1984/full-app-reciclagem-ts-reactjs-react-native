8aa301935fcf53da77eedd72811dccd7b5a795d6
Rodrigo <rodrigodiasf@gmail.com>
2020-06-06 15:26:58 +0100

    Adicionada integração com maps

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Feather';
-import {Container, ContainerIcon} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {SvgUri} from 'react-native-svg';
+import {
+  Container,
+  ContainerIcon,
+  Title,
+  Description,
+  MapContainer,
+  Map,
+  ItemsContainer,
+  Item,
+} from './styles';
 
 const Points = () => {
+  const navigation = useNavigation();
   return (
     <>
       <Container>
-        <ContainerIcon>
+        <ContainerIcon
+          onPress={() => {
+            navigation.goBack();
+          }}>
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </ContainerIcon>
+        <Title>Bem vindo.</Title>
+        <Description>Encontre no mapa um ponto de coleta</Description>
+        <MapContainer>
+          <Map />
+        </MapContainer>
       </Container>
+      <ItemsContainer>
+        <Item>
+          <SvgUri
+            width={42}
+            height={42}
+            uri="http://192.168.1.104:3333/uploads/lampadas.svg"
+          />
+        </Item>
+      </ItemsContainer>
     </>
   );
 };
--- mobile/src/pages/Points/styles.ts
+++ mobile/src/pages/Points/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
 import {TouchableOpacity} from 'react-native';
+import MapView from 'react-native-maps';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex: 1;
   padding: 0px 32px 0px 32px;
   padding-top: 40px;
@@ -9,20 +10,20 @@ export const Container = styled.SafeAreaView`
 
 export const ContainerIcon = styled(TouchableOpacity)``;
 
-export const title = styled.Text`
-  font-size: 20;
+export const Title = styled.Text`
+  font-size: 20px;
   font-family: 'Ubuntu_700Bold';
   margin-top: 24px;
 `;
 
-export const description = styled.Text`
+export const Description = styled.Text`
   color: #6c6c80;
   font-size: 16px;
-  margin-top: 4;
+  margin-top: 4px;
   font-family: 'Roboto_400Regular';
 `;
 
-export const mapContainer = styled.View`
+export const MapContainer = styled.View`
   flex: 1;
   width: 100%;
   border-radius: 10px;
@@ -30,52 +31,52 @@ export const mapContainer = styled.View`
   margin-top: 16px;
 `;
 
-export const map = styled.View`
+export const Map = styled(MapView)`
   width: 100%;
   height: 100%;
 `;
 
-export const mapMarker = styled.View`
-  width: 90;
-  height: 80;
+export const MapMarker = styled.View`
+  width: 90px;
+  height: 80px;
 `;
 
-export const mapMarkerContainer = styled.View`
-  width: 90;
-  height: 70;
+export const MapMarkerContainer = styled.View`
+  width: 90px;
+  height: 70px;
   background-color: #34cb79;
   flex-direction: column;
-  border-radius: 8;
+  border-radius: 8px;
   overflow: hidden;
   align-items: center;
 `;
 
-export const mapMarkerImage = styled.Image`
-  width: 90;
-  height: 45;
+export const MapMarkerImage = styled.Image`
+  width: 90px;
+  height: 45px;
 `;
 
-export const mapMarkerTitle = styled.Text`
+export const MapMarkerTitle = styled.Text`
   flex: 1;
   font-family: 'Roboto_400Regular';
   color: #fff;
-  font-size: 13;
-  line-height: 23;
+  font-size: 13px;
+  line-height: 23px;
 `;
 
-export const itemsContainer = styled.View`
+export const ItemsContainer = styled.View`
   flex-direction: row;
   margin-top: 16px;
-  margin-bottom: 32;
+  margin-bottom: 32px;
 `;
 
-export const item = styled.View`
-  background-color: '#fff';
-  border-width: 2;
-  border-color: '#eee';
-  height: 120;
-  width: 120;
-  border-radius: 8;
+export const Item = styled(TouchableOpacity)`
+  background-color: #fff;
+  border-width: 2px;
+  border-color: #eee;
+  height: 120px;
+  width: 120px;
+  border-radius: 8px;
   padding: 0px 16px 0px 32px;
   padding-top: 20px;
   padding-bottom: 16px;
@@ -85,13 +86,13 @@ export const item = styled.View`
   text-align: center;
 `;
 
-export const selectedItem = styled.View`
+export const SelectedItem = styled.View`
   border-color: #34cb79;
-  border-width: 2;
+  border-width: 2px;
 `;
 
-export const itemTitle = styled.Text`
+export const ItemTitle = styled.Text`
   font-family: 'Roboto_400Regular';
   text-align: center;
-  font-size: 13;
+  font-size: 13px;
 `;
